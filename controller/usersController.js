99737d90ad922db497e01eee695a9b8701c309e5
Matthew <matthew@stateofmind.agency>
2020-02-05 20:27:09 +0000

    Added small features in routes.

--- controller/usersController.js
+++ controller/usersController.js
@@ -1,8 +1,10 @@
+// user comments
+
 const User = require('../models/userModel')
 
 exports.getUsers = async (req, res) => {
     try {
-        const users = await User.find(req.query)
+        const users =  User.find(req.query)
         res.status(200).json({
             status: "success",
             message: "This is the route for all users",
